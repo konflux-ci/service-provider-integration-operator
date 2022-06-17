5e2aec9d8ad70d854b08e52c1ae42023a1d8d1d8
Michal Vala <mvala@redhat.com>
2022-06-17 12:55:21 +0200

    fix?

--- pkg/serviceprovider/quay/quay.go
+++ pkg/serviceprovider/quay/quay.go
@@ -74,14 +74,14 @@ func newQuay(factory *serviceprovider.Factory, _ string) (serviceprovider.Servic
 			},
 			MetadataProvider: mp,
 			MetadataCache:    &cache,
-			RepoHostParser: serviceprovider.RepoHostParserFunc(func(repoUrl string) (string, error) {
+			RepoHostParser: func(repoUrl string) (string, error) {
 				schemeIndex := strings.Index(repoUrl, "://")
 				if schemeIndex == -1 {
 					repoUrl = "https://" + repoUrl
 				}
 
 				return serviceprovider.RepoHostFromUrl(repoUrl)
-			}),
+			},
 		},
 		httpClient:       factory.HttpClient,
 		tokenStorage:     factory.TokenStorage,
