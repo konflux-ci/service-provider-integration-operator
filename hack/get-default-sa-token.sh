c07c47cd42edbf66cdfef1ac5659a8672f9e9388
Michal Vala <mvala@redhat.com>
2022-07-07 16:04:25 +0200

    get token of default SA in default namespace in kubernetes 1.24+

--- hack/get-default-sa-token.sh
+++ hack/get-default-sa-token.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-SECRET_NAME=$(kubectl get serviceaccount default -n default -o jsonpath='{.secrets[0].name}')
-
-kubectl get secret "${SECRET_NAME}" -o jsonpath='{.data.token}' | base64 -d
+kubectl create token default -n default
