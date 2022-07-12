3b4649b2bcc089d0b6cd4d0478d4e10c550a0080
Michal Vala <mvala@redhat.com>
2022-07-12 14:32:19 +0200

    update status of spiaccesscheck when token lookup fails (#180)

--- pkg/serviceprovider/quay/quay_test.go
+++ pkg/serviceprovider/quay/quay_test.go
@@ -514,7 +514,7 @@ func TestCheckRepositoryAccess(t *testing.T) {
 		assert.NotEmpty(t, status.ErrorMessage)
 	})
 
-	t.Run("lookup failed", func(t *testing.T) {
+	t.Run("lookup failed public", func(t *testing.T) {
 		failingLookup := lookupMock
 		failingLookup.TokenFilter = tokenFilterMock{matchesFunc: func(ctx context.Context, matchable serviceprovider.Matchable, token *api.SPIAccessToken) (bool, error) {
 			return false, errors.New("intentional failure")
@@ -530,14 +530,40 @@ func TestCheckRepositoryAccess(t *testing.T) {
 
 		status, err := quay.CheckRepositoryAccess(context.TODO(), cl, accessCheck)
 
-		assert.Error(t, err)
+		assert.NoError(t, err)
+		assert.NotNil(t, status)
+		assert.True(t, status.Accessible)
+		assert.Equal(t, api.SPIRepoTypeContainerRegistry, status.Type)
+		assert.Equal(t, api.ServiceProviderTypeQuay, status.ServiceProvider)
+		assert.Equal(t, api.SPIAccessCheckAccessibilityPublic, status.Accessibility)
+		assert.Empty(t, status.ErrorMessage)
+		assert.Empty(t, status.ErrorReason)
+	})
+
+	t.Run("lookup failed nonpublic", func(t *testing.T) {
+		failingLookup := lookupMock
+		failingLookup.TokenFilter = tokenFilterMock{matchesFunc: func(ctx context.Context, matchable serviceprovider.Matchable, token *api.SPIAccessToken) (bool, error) {
+			return false, errors.New("intentional failure")
+		}}
+
+		quay := &Quay{
+			httpClient: httpClientMock{doFunc: func(req *http.Request) (*http.Response, error) {
+				return &http.Response{StatusCode: http.StatusNotFound, Body: io.NopCloser(strings.NewReader(publicRepoResponseJson))}, nil
+			}},
+			lookup:       failingLookup,
+			tokenStorage: ts,
+		}
+
+		status, err := quay.CheckRepositoryAccess(context.TODO(), cl, accessCheck)
+
+		assert.NoError(t, err)
 		assert.NotNil(t, status)
 		assert.False(t, status.Accessible)
 		assert.Equal(t, api.SPIRepoTypeContainerRegistry, status.Type)
 		assert.Equal(t, api.ServiceProviderTypeQuay, status.ServiceProvider)
 		assert.Equal(t, api.SPIAccessCheckAccessibilityUnknown, status.Accessibility)
-		assert.Equal(t, api.SPIAccessCheckErrorUnknownError, status.ErrorReason)
 		assert.NotEmpty(t, status.ErrorMessage)
+		assert.Equal(t, api.SPIAccessCheckErrorTokenLookupFailed, status.ErrorReason)
 	})
 
 	t.Run("no token data", func(t *testing.T) {
