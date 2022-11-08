49cfd324d06962e69c62f548d1b6793aad6be46c
Sergii Kabashniuk <skabashniuk@redhat.com>
2022-11-08 17:58:26 +0200

    Dashboard update

--- oauth/metrics/metrics.go
+++ oauth/metrics/metrics.go
@@ -1,6 +1,7 @@
 package metrics
 
 import (
+	"github.com/redhat-appstudio/service-provider-integration-operator/pkg/spi-shared/config"
 	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -11,8 +12,10 @@ import (
 func OAuthServiceInstrumentMetricHandler(reg prometheus.Registerer, handler http.Handler) http.Handler {
 	reqCounter := promauto.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "spi_oauth_service_requests_total",
-			Help: "Total number of spi oauth service requests by HTTP code.",
+			Namespace: config.MetricsNamespace,
+			Subsystem: config.MetricsSubsystem,
+			Name:      "oauth_service_requests_total",
+			Help:      "Total number of spi oauth service requests by HTTP code.",
 		},
 		[]string{"code", "method"},
 	)
