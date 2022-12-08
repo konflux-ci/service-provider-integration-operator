5b01be0297f75e1bc569ec52547966e3f13564d8
Sergii Kabashniuk <skabashniuk@redhat.com>
2022-12-08 17:48:26 +0200

    fixup! fixup! fixup! fixup! Created CallBackRoutePath and AuthenticateRoutePath constants
    Signed-off-by: Sergii Kabashniuk <skabashniuk@redhat.com>

--- pkg/serviceprovider/oauth/oauth.go
+++ pkg/serviceprovider/oauth/oauth.go
@@ -1,5 +1,7 @@
 package oauth
 
+// Constants that are used both in the operator and OAuth service
+// Placed here to keep only a reference to operator packages in OAuth service and not vice versa.
 var (
 	CallBackRoutePath     = "/oauth/callback"
 	AuthenticateRoutePath = "/oauth/authenticate"
