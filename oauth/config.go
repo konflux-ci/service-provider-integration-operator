2cd487b8d10932d791cf0dbd418fc144bc224ce5
Sergii Kabashniuk <skabashniuk@redhat.com>
2023-02-11 16:30:45 +0200

    Added console.redhat.com as allowed origin

--- oauth/config.go
+++ oauth/config.go
@@ -25,7 +25,7 @@ type OAuthServiceCliArgs struct {
 	config.LoggingCliArgs
 	vaultstorage.VaultCliArgs
 	ServiceAddr     string `arg:"--service-addr, env" default:"0.0.0.0:8000" help:"Service address to listen on"`
-	AllowedOrigins  string `arg:"--allowed-origins, env" default:"https://console.dev.redhat.com,https://prod.foo.redhat.com:1337" help:"Comma-separated list of domains allowed for cross-domain requests"`
+	AllowedOrigins  string `arg:"--allowed-origins, env" default:"https://console.dev.redhat.com,https://prod.foo.redhat.com:1337,https://console.redhat.com" help:"Comma-separated list of domains allowed for cross-domain requests"`
 	KubeConfig      string `arg:"--kubeconfig, env" default:"" help:""`
 	KubeInsecureTLS bool   `arg:"--kube-insecure-tls, env" default:"false" help:"Whether is allowed or not insecure kubernetes tls connection."`
 	ApiServer       string `arg:"--api-server, env:API_SERVER" default:"" help:"host:port of the Kubernetes API server to use when handling HTTP requests"`
