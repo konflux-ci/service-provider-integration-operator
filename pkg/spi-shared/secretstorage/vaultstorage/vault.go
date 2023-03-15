d1ab5785d8885cc87eda7f865446296cd6058b8f
Lukas Krejci <lkrejci@redhat.com>
2023-03-15 17:33:16 +0100

    Handle legacy data in Vault transparently and gradually migrate it.

--- pkg/spi-shared/secretstorage/vaultstorage/vault.go
+++ pkg/spi-shared/secretstorage/vaultstorage/vault.go
@@ -17,9 +17,11 @@ package vaultstorage
 import (
 	"context"
 	"encoding/base64"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
+	"reflect"
 	"strconv"
 
 	"github.com/hashicorp/go-hclog"
@@ -47,7 +49,7 @@ const vaultDataPathFormat = "%s/data/%s/%s"
 var (
 	VaultError             = errors.New("error in Vault")
 	noAuthInfoInVaultError = errors.New("no auth info returned from Vault")
-	unexpectedDataError    = errors.New("unexpected data")
+	UnexpectedDataError    = errors.New("unexpected data")
 	unspecifiedStoreError  = errors.New("failed to store the token, no error but returned nil")
 
 	vaultRequestCountMetric = prometheus.NewCounterVec(prometheus.CounterOpts{
@@ -121,7 +123,7 @@ func (v *VaultSecretStorage) Initialize(ctx context.Context) error {
 
 func (v *VaultSecretStorage) Store(ctx context.Context, id secretstorage.SecretID, bytes []byte) error {
 	data := map[string]interface{}{
-		// yes, the data HAS TO be a map
+		// yes, the data HAS TO be a JSON object (or serializable thereto). Even if a string is a valid JSON value, it gives Vault fits :)
 		"data": map[string]interface{}{
 			"bytes": base64.StdEncoding.EncodeToString(bytes),
 		},
@@ -162,27 +164,22 @@ func (v *VaultSecretStorage) Get(ctx context.Context, id secretstorage.SecretID)
 		lg.Info(w)
 	}
 
-	dataField, ok := secret.Data["data"]
-	if !ok {
-		return nil, fmt.Errorf("%w: data field not present in Vault response", unexpectedDataError)
-	}
-	dataMap, ok := dataField.(map[string]interface{})
-	if !ok {
-		return nil, fmt.Errorf("%w: data field not a map", unexpectedDataError)
+	bytes, legacy, err := extractData(secret.Data)
+	if err != nil {
+		return nil, fmt.Errorf("failed to extract the data from Vault response: %w", err)
 	}
 
-	bytesField, ok := dataMap["bytes"]
-	if !ok {
-		return nil, fmt.Errorf("%w: bytes field not present", unexpectedDataError)
+	if legacy {
+		// we're dealing with legacy data. Let's convert right now to the new format so that we do
+		// incremental migration to the new data format..
+		if err := v.Store(ctx, id, bytes); err != nil {
+			// we have the correct bytes to return to the caller, we just failed to persist.
+			// Let's go on and just log that we failed to update to the new format.
+			// Maybe it's gonna work the next time?
+			lg.Error(err, "failed to migrate to the new data format, but was able to at least retrieve and decode the legacy data")
+		}
 	}
-	bytesStr, ok := bytesField.(string)
-	if !ok {
-		return nil, fmt.Errorf("%w: bytes field is not string", unexpectedDataError)
-	}
-	bytes, err := base64.StdEncoding.DecodeString(bytesStr)
-	if err != nil {
-		return nil, fmt.Errorf("bytes field not base64 encoded: %w", err)
-	}
+
 	return bytes, nil
 }
 
@@ -269,4 +266,91 @@ func (v *VaultSecretStorage) initMetrics(ctx context.Context) error {
 	}
 
 	return nil
+}
+
+// extractData trie to extract the data from the Vault response. It supports the new byte-array-based
+// storage format as well as the legacy format which serialized strictly only api.Token objects.
+//
+// If the legacy format is detected, the legacy output parameter is set to true, otherwise it is false
+func extractData(responseData map[string]interface{}) (data []byte, legacy bool, err error) {
+	data, err = extractByteData(responseData)
+	if err != nil {
+		data, err = extractLegacyTokenData(responseData)
+		legacy = true
+	}
+	return
+}
+
+func extractByteData(responseData map[string]interface{}) ([]byte, error) {
+	dataField, ok := responseData["data"]
+	if !ok {
+		return nil, fmt.Errorf("%w: data field not present in Vault response", UnexpectedDataError)
+	}
+	dataMap, ok := dataField.(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("%w: data field not a map", UnexpectedDataError)
+	}
+
+	bytesField, ok := dataMap["bytes"]
+	if !ok {
+		return nil, fmt.Errorf("%w: bytes field not present", UnexpectedDataError)
+	}
+	bytesStr, ok := bytesField.(string)
+	if !ok {
+		return nil, fmt.Errorf("%w: bytes field is not string", UnexpectedDataError)
+	}
+	bytes, err := base64.StdEncoding.DecodeString(bytesStr)
+	if err != nil {
+		return nil, fmt.Errorf("bytes field not base64 encoded: %w", err)
+	}
+	return bytes, nil
+}
+
+func extractLegacyTokenData(responseData map[string]interface{}) ([]byte, error) {
+	dataField, ok := responseData["data"]
+	if !ok {
+		return nil, fmt.Errorf("%w: data field not present in Vault response", UnexpectedDataError)
+	}
+
+	dataMap, ok := dataField.(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("%w: data field not a map", UnexpectedDataError)
+	}
+
+	// the data stored in Vault used to be the api.Token object itself (not its serialized form).
+	// So let's check that it has all the fields in the right format and re-shape it into the serialized
+	// form again.
+
+	if !legacyFieldNotPresentOrHasType(dataMap, "username", reflect.TypeOf("")) {
+		return nil, fmt.Errorf("%w: legacy data doesn't have username field with the right type", UnexpectedDataError)
+	}
+	if !legacyFieldNotPresentOrHasType(dataMap, "access_token", reflect.TypeOf("")) {
+		return nil, fmt.Errorf("%w: legacy data doesn't have access_token field with the right type", UnexpectedDataError)
+	}
+	if !legacyFieldNotPresentOrHasType(dataMap, "token_type", reflect.TypeOf("")) {
+		return nil, fmt.Errorf("%w: legacy data doesn't have token_type field with the right type", UnexpectedDataError)
+	}
+	if !legacyFieldNotPresentOrHasType(dataMap, "refresh_token", reflect.TypeOf("")) {
+		return nil, fmt.Errorf("%w: legacy data doesn't have refresh_token field with the right type", UnexpectedDataError)
+	}
+	if !legacyFieldNotPresentOrHasType(dataMap, "expiry", reflect.TypeOf(json.Number(""))) {
+		return nil, fmt.Errorf("%w: legacy data doesn't have expiry field with the right type", UnexpectedDataError)
+	}
+
+	// ok, the data has the legacy format, so let's marshal it back to []byte
+	bytes, err := json.Marshal(dataMap)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert the legacy data into byte array: %w", err)
+	}
+
+	return bytes, nil
+}
+
+func legacyFieldNotPresentOrHasType(object map[string]interface{}, fieldName string, fieldType reflect.Type) bool {
+	val, ok := object[fieldName]
+	if !ok || val == nil {
+		return true
+	}
+
+	return reflect.TypeOf(val).AssignableTo(fieldType)
 }
