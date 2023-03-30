6041e6d09390aff09cf6b8bbf6a2cc9a2412fa01
Lukas Krejci <lkrejci@redhat.com>
2023-03-30 11:45:42 +0200

    Improve how the audit log is used and remove the superfluous "namespace" value that is always taken from the context (where the controller runtime puts the currently reconciled secret).

--- controllers/tokenupload_controller.go
+++ controllers/tokenupload_controller.go
@@ -100,15 +100,17 @@ func (r *TokenUploadReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 			AccessToken: string(uploadSecret.Data["tokenData"]),
 		}
 
-		logs.AuditLog(ctx).Info("manual token upload initiated", "namespace", uploadSecret.Namespace, "SPIAccessToken.name", accessToken.Name)
+		auditLog := logs.AuditLog(ctx).WithValues("SPIAccessToken.name", accessToken.Name)
+
+		auditLog.Info("manual token upload initiated")
 		// Upload Token, it will cause update SPIAccessToken State as well
 		err = r.TokenStorage.Store(ctx, accessToken, &token)
 		if err != nil {
 			r.logError(ctx, uploadSecret, fmt.Errorf("failed to store the token: %w", err), lg)
-			logs.AuditLog(ctx).Error(err, "manual token upload failed", "namespace", uploadSecret.Namespace, "SPIAccessToken.name", accessToken.Name)
+			auditLog.Error(err, "manual token upload failed")
 			continue
 		}
-		logs.AuditLog(ctx).Info("manual token upload completed", "namespace", uploadSecret.Namespace, "SPIAccessToken.name", accessToken.Name)
+		auditLog.Info("manual token upload completed")
 
 		r.tryDeleteEvent(ctx, uploadSecret.Name, req.Namespace, lg)
 	}
