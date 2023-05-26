d26636ab96bd0bc0d5047cf2d405baa314357b22
Sergii Kabashniuk <skabashniuk@redhat.com>
2023-05-26 10:23:04 +0300

    fixup! fixup! Change the OAuth service configuration
    Signed-off-by: Sergii Kabashniuk <skabashniuk@redhat.com>

--- pkg/spi-shared/config/config_test.go
+++ pkg/spi-shared/config/config_test.go
@@ -31,7 +31,6 @@ func TestRead(t *testing.T) {
 	SetupCustomValidations(CustomValidationOptions{AllowInsecureURLs: true})
 	t.Run("all supported service providers are set", func(t *testing.T) {
 		configFileContent := `
-redirectProxyUrl: https:/localhost:8080
 serviceProviders:
 - type: GitHub
   clientId: "123"
@@ -48,7 +47,6 @@ serviceProviders:
 
 		assert.Equal(t, "blabol", cfg.BaseUrl)
 		assert.Len(t, cfg.ServiceProviders, len(SupportedServiceProviderTypes))
-		assert.Equal(t, "https:/localhost:8080", cfg.RedirectProxyUrl)
 	})
 
 	t.Run("all supported service providers are set even if config is empty", func(t *testing.T) {
