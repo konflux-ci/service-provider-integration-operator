d061e7f28073972765e7b4e248fbea70d9126582
Sergii Kabashniuk <skabashniuk@redhat.com>
2023-05-30 08:35:55 +0300

    fixup! Merge remote-tracking branch 'upstream/main' into redirectProxy
    Signed-off-by: Sergii Kabashniuk <skabashniuk@redhat.com>

--- cmd/oauth/oauthcli/cli.go
+++ cmd/oauth/oauthcli/cli.go
@@ -21,11 +21,11 @@ import (
 type OAuthServiceCliArgs struct {
 	cmd.CommonCliArgs
 	cmd.LoggingCliArgs
-	ServiceAddr      string `arg:"--service-addr, env" default:"0.0.0.0:8000" help:"Service address to listen on"`
-	AllowedOrigins   string `arg:"--allowed-origins, env" default:"https://console.redhat.com,https://console.stage.redhat.com,https://console.dev.redhat.com,https://prod.foo.redhat.com:1337" help:"Comma-separated list of domains allowed for cross-domain requests"`
-	KubeConfig       string `arg:"--kubeconfig, env" default:"" help:""`
-	KubeInsecureTLS  bool   `arg:"--kube-insecure-tls, env" default:"false" help:"Whether is allowed or not insecure kubernetes tls connection."`
-	ApiServer        string `arg:"--api-server, env:API_SERVER" default:"" help:"host:port of the Kubernetes API server to use when handling HTTP requests"`
-	ApiServerCAPath  string `arg:"--ca-path, env:API_SERVER_CA_PATH" default:"" help:"the path to the CA certificate to use when connecting to the Kubernetes API server"`
-	RedirectProxyUrl string `arg:"--redirect-proxy-url, env:REDIRECT_PROXY_URL" default:"" help:"the URL of OAuth redirection proxy used in the tests to maintain predictable callback URL"`
+	ServiceAddr           string `arg:"--service-addr, env" default:"0.0.0.0:8000" help:"Service address to listen on"`
+	AllowedOrigins        string `arg:"--allowed-origins, env" default:"https://console.redhat.com,https://console.stage.redhat.com,https://console.dev.redhat.com,https://prod.foo.redhat.com:1337" help:"Comma-separated list of domains allowed for cross-domain requests"`
+	KubeConfig            string `arg:"--kubeconfig, env" default:"" help:""`
+	KubeInsecureTLS       bool   `arg:"--kube-insecure-tls, env" default:"false" help:"Whether is allowed or not insecure kubernetes tls connection."`
+	ApiServer             string `arg:"--api-server, env:API_SERVER" default:"" help:"host:port of the Kubernetes API server to use when handling HTTP requests"`
+	ApiServerCAPath       string `arg:"--ca-path, env:API_SERVER_CA_PATH" default:"" help:"the path to the CA certificate to use when connecting to the Kubernetes API server"`
+	OAuthRedirectProxyUrl string `arg:"--oauth-redirect-proxy-url, env:OAUTH_REDIRECT_PROXY_URL" default:"" help:"the URL of OAuth redirection proxy used in the tests to maintain predictable callback URL"`
 }
