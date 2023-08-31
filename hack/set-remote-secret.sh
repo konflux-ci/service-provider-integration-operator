5fb45ed7a9cb2d0a3c876e746aeedb18baab6f28
Max Shaposhnyk <mshaposh@redhat.com>
2023-08-31 12:54:36 +0300

    Add cert generation & webhooks deployment for minikube profile (#692)
    ---------
    
    Signed-off-by: Max Shaposhnyk <mshaposh@redhat.com>
    Co-authored-by: Sergii Kabashniuk <skabashniuk@redhat.com>

--- hack/set-remote-secret.sh
+++ hack/set-remote-secret.sh
@@ -17,4 +17,4 @@ go get  github.com/redhat-appstudio/remote-secret@"$REMOTE_SECRET_COMMIT_ID"
 go mod tidy
 
 # shellcheck disable=SC2016
-sed -i -e 's|\(https://github.com/redhat-appstudio/remote-secret/config/overlays/minikube_vault?ref=\)\(.*\)|\1'"$REMOTE_SECRET_COMMIT_ID"'|' config/remotesecret/overlays/minikube_vault/kustomization.yaml
+sed -i -e 's|\(https://github.com/redhat-appstudio/remote-secret/config/bases/vault?ref=\)\(.*\)|\1'"$REMOTE_SECRET_COMMIT_ID"'|' config/remotesecret/overlays/minikube_vault/kustomization.yaml
