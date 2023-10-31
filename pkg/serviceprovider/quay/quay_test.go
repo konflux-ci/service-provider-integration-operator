771ea9dfcad399084ea801a4cb890d72975a8106
Sergii Kabashniuk <skabashniuk@redhat.com>
2023-10-31 16:35:16 +0200

    fixup! Fixup
    Signed-off-by: Sergii Kabashniuk <skabashniuk@redhat.com>

--- pkg/serviceprovider/quay/quay_test.go
+++ pkg/serviceprovider/quay/quay_test.go
@@ -744,7 +744,7 @@ func mockK8sClient(objects ...client.Object) client.WithWatch {
 	utilruntime.Must(corev1.AddToScheme(sch))
 	utilruntime.Must(api.AddToScheme(sch))
 	utilruntime.Must(v1beta1.AddToScheme(sch))
-	return fake.NewClientBuilder().WithScheme(sch).WithObjects(objects...).Build()
+	return fake.NewClientBuilder().WithScheme(sch).WithObjects(objects...).WithStatusSubresource(objects...).Build()
 }
 
 type tokenFilterMock struct {
